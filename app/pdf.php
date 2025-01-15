1f09d62bc4af56c0d0324fd52e0934976dd54258
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-15 13:39:49 +0100

    done

--- app/pdf.php
+++ app/pdf.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../fpdf/fpdf.php';
+require_once '../fpdf186/fpdf.php';
 
 // Verifica si se pasó el parámetro 'ranking'
 if (isset($_GET['ranking'])) {
@@ -15,11 +15,16 @@ if (isset($_GET['ranking'])) {
     $pdf->Cell(0, 10, 'Ranking', 0, 1, 'C');
     $pdf->Ln(10);
 
+   $pdf->Cell(40, 10, 'Posicion', 1, 0, 'C');  
+    $pdf->Cell(90, 10, 'Nombre', 1, 0, 'C');     
+    $pdf->Cell(40, 10, 'Puntuacion', 1, 1, 'C'); 
+    $pdf->SetFont('Arial', '', 12);
+
     // Crear la tabla de rankings
     foreach ($ranking as $index => $fila) {
-        $pdf->Cell(40, 10, 'Posicion: ' . ($index + 1), 1);
-        $pdf->Cell(90, 10, 'Nombre: ' . $fila['nickname'], 1);
-        $pdf->Cell(40, 10, 'Puntuacion: ' . $fila['puntuacion'], 1);
+        $pdf->Cell(40, 10, ($index + 1), 1);
+        $pdf->Cell(90, 10, $fila['nickname'], 1);
+        $pdf->Cell(40, 10, $fila['puntuacion'], 1);
         $pdf->Ln();
     }
 
@@ -28,4 +33,3 @@ if (isset($_GET['ranking'])) {
 } else {
     echo "No se encontraron datos para generar el PDF.";
 }
-?>
